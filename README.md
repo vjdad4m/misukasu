bf64a6ac8e485b8519fa1d3c7887c9542feebfcb
Adam Vajda <vjdad4m@gmail.com>
2022-11-13 22:03:15 +0000

    benchmark test

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-## MISUKASU
+# MISUKASU
 
 Radar-based pose estimation with additional features
 
-Requires: WalabotAPI, PyTorch, OpenCv
+Requires: [walabot](https://walabot.com/), numpy, opencv
+
+## sensor apis
+
+misukasu provides easy-to-use apis for the used sensors.
+
+```python
+from misukasu.sensor import SensorCamera
+from misukasu.sensor import SensorRadar
+
+camera = SensorCamera()
+radar = SensorRadar()
+
+while True:
+	camera_latest = camera.get_frame()
+	radar_latest = radar.get_frame()
+
+camera.release()
+radar.release()
+```
---
+++ test/benchmark.py
@@ -0,0 +1,48 @@
+import time
+import argparse
+
+from misukasu.sensor import SensorCamera, SensorRadar
+
+def test_camera(test_duration):
+    cam = SensorCamera()
+    n_frames = 0
+
+    start_time = time.time()
+    while time.time() - start_time < test_duration:
+        frame = cam.get_frame()
+        n_frames += 1
+
+    cam.release()
+    return n_frames
+
+def test_radar(test_duration):
+    radar = SensorRadar()
+    n_frames = 0
+
+    start_time = time.time()
+    while time.time() - start_time < test_duration:
+        frame = radar.get_frame()
+        n_frames += 1
+
+    radar.release()
+    return n_frames
+
+def print_stats(n_frames, duration):
+    print('received', n_frames, 'frames')
+    print('test took', duration, 'seconds')
+    print('resulting in', n_frames / duration, 'fps')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--duration', type = int, default = 10)
+    args = parser.parse_args()
+    test_duration = args.duration
+
+    print('selected test duration:', test_duration, 'seconds')
+    print('running camera benchmark...')
+    frames = test_camera(test_duration)
+    print_stats(frames, test_duration)
+    print('running radar benchmark...')
+    frames = test_radar(test_duration)
+    print_stats(frames, test_duration)
